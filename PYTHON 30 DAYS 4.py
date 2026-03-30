f60375e8f3efa81688e1260dc94949288a9f07a8
NANTHINI P <nandhuvarshi01@gmail.com>
2026-03-30 15:25:02 +0530

    DAY 4-Largest Number Python Program

---
+++ PYTHON 30 DAYS 4.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 1st number:"))
+c=int(input("Enter 1st number:"))
+if(a>b and a>c):
+    print("The largest element a is",a)
+elif(b>c):
+    print("The largest element b is",b)
+else:
+    print("The largest element c is",c)
